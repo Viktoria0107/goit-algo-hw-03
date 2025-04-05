1d7b96b367d80e953d47ab70e691fe67c2da5fbf
Viktoria0107 <viktorija.panchenko@gmail.com>
2025-04-05 23:42:19 +0300

    add_lottery.py

---
+++ lottery.py
@@ -0,0 +1,10 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    list_lottery_numbers = []
+    if min >= 1 and max <= 1000:
+        list_lottery_numbers.append(sorted(random.sample(range(min, max + 1), quantity)))
+        return list_lottery_numbers
+    
+
+print (get_numbers_ticket(2, 50, 5))
