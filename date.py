1affda872ac5b2084356ac366a058ebab5bbd4d6
Viktoria0107 <viktorija.panchenko@gmail.com>
2025-04-06 20:40:49 +0300

    uppdate

--- date.py
+++ date.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 def get_days_from_today(date):
     try:
-        date_new = datetime.strptime(date, "%Y.%m.%d").date()
+        date_new = datetime.strptime(date, "%Y-%m-%d").date()
         date_now = datetime.today().date()
-        days = date_now - date_new
-        return days
+        quantity_days = date_now - date_new
+        return quantity_days.days
     except ValueError:
-        raise ValueError("Format is expected YYYY.MM.DD")
-
+        raise ValueError("Format is expected YYYY-MM-DD")
--- number_phone.py
+++ number_phone.py
@@ -2,11 +2,11 @@ import re
 def normalize_phone(phone_number):
      phone_number = re.sub(r"[^/0-9+]", "", phone_number)
      if phone_number.startswith("38"):
-         return "+" + phone_number
+        return "+" + phone_number
      if phone_number.startswith("+380"):
-          return phone_number
+        return phone_number
      else:
-          return "+38" + phone_number 
+        return "+38" + phone_number 
 
 
 
