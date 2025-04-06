1914590456b067ef9931778bca1f60aec97eef86
Viktoria0107 <viktorija.panchenko@gmail.com>
2025-04-06 11:32:47 +0300

    update

--- number_phone.py
+++ number_phone.py
@@ -3,8 +3,9 @@ def normalize_phone(phone_number):
      phone_number = re.sub(r"[^/0-9+]", "", phone_number)
      if phone_number.startswith("38"):
          return "+" + phone_number
-     if phone_number.startswith("+38"):
+     if phone_number.startswith("+380"):
           return phone_number
      else:
           return "+38" + phone_number 
+
 
